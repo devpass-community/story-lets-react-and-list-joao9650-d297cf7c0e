47dc6f0f6bcd2b70496cb82eaa3e0a9776cf5bea
jvcenedeze <jvcenedeze@hotmail.com>
2023-06-05 19:32:23 -0300

    feat: smartphones list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import Container from './components/Container';
+import products from "./products.json";
+import Container from "./components/Container";
 
 const Item = ({ item }) => {
   return (
-    <li className='item' data-testid='product-item'>
-      { /* TODO */ }
-      <img src="thumbnail here"></img>
-      <h4> title here </h4>
-      <p> description here </p>
-      <span> brand here </span>
-      <span> price here </span>
+    <li className="item" data-testid="product-item">
+      <img src={item.images[item.images.length - 1]} alt={item.title}></img>
+      <h4> {item.title} </h4>
+      <p> {item.description} </p>
+      <span> {item.brand} </span>
+      <span> {item.price} </span>
     </li>
-  )
-}
+  );
+};
 
 function App() {
+  const smartphones = products.filter(
+    (product) => product.category === "smartphones"
+  );
   return (
     <Container>
-      <ul className='list' data-testid='product-list'>
-        { /* TODO */ }
-        <Item />
+      <ul className="list" data-testid="product-list">
+        {smartphones
+          ? smartphones.map((smartphone) => (
+              <Item key={smartphone.id} item={smartphone} />
+            ))
+          : null}
       </ul>
     </Container>
   );
